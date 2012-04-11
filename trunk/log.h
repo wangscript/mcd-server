3c6d3e66108ea4792ef0ac67ae425eca2e9fab08
jlijian3@gmail.com <jlijian3@gmail.com@b895b7f6-2ae7-8340-2402-2b734e08ea3e>
2012-04-11 16:17:10 +0000

    add memcached-like server framework

---
+++ trunk/log.h
@@ -0,0 +1,10 @@
+#ifndef _CWQ_LOG_H
+#define _CWQ_LOG_H
+
+#define dlog1(args...)		fprintf(stderr, args)
+#define dlog2		          dlog1
+#define dlog3             dlog1		
+#define dlog4             dlog1	
+
+#endif
+
